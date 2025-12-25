b9d7e9f21b9a1add86f010f6760f4f69907a68a5
James Zhang <51384498+ZuseGD@users.noreply.github.com>
2025-12-24 19:18:14 -0800

    told Next.js to generate static HTML files and disable the image optimizer.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',  // <--- Forces static HTML export
+  images: {
+    unoptimized: true, // <--- Required for GitHub Pages
+  },
 }
 
 module.exports = nextConfig
